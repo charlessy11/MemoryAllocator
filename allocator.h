e1070f1864a1c6e9558755e728db355ffc2c8099
clsy <clsy@usfca.edu>
2021-04-12 21:02:24 -0700

    Add Documentation

--- allocator.h
+++ allocator.h
@@ -10,20 +10,120 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-/* -- Helper functions -- */
+/**
+ * Given a free block, this function will split it into two pieces and update
+ * the linked list.
+ *
+ * @param block the block to split
+ * @param size new size of the first block after the split is complete,
+ * including header sizes. The size of the second block will be the original
+ * block's size minus this parameter.
+ *
+ * @return address of the resulting second block (the original address will be
+ * unchanged) or NULL if the block cannot be split.
+ */
 struct mem_block *split_block(struct mem_block *block, size_t size);
+
+/**
+ * Given a free block, this function attempts to merge it with neighboring
+ * blocks --- both the previous and next neighbors --- and update the linked
+ * list accordingly.
+ *
+ * @param block the block to merge
+ *
+ * @return address of the merged block or NULL if the block cannot be merged.
+ */
 struct mem_block *merge_block(struct mem_block *block);
+
+/**
+* Finds a block of memory that we can reuse
+*
+* @param size the size of the block
+*
+* @return found if suitable block is found or null if otherwise
+*/
 void *reuse(size_t size);
+
+/**
+ * Given a block size (header + data), locate a suitable location using the
+ * first fit free space management algorithm.
+ *
+ * @param size size of the block (header + data)
+ */
 void *first_fit(size_t size);
+
+/**
+ * Given a block size (header + data), locate a suitable location using the
+ * worst fit free space management algorithm. If there are ties (i.e., you find
+ * multiple worst fit candidates with the same size), use the first candidate
+ * found.
+ *
+ * @param size size of the block (header + data)
+ */
 void *worst_fit(size_t size);
+
+/**
+ * Given a block size (header + data), locate a suitable location using the
+ * best fit free space management algorithm. If there are ties (i.e., you find
+ * multiple best fit candidates with the same size), use the first candidate
+ * found.
+ *
+ * @param size size of the block (header + data)
+ */
 void *best_fit(size_t size);
+
+/**
+ * Prints out the current memory state, including both the regions and blocks.
+ * Entries are printed in order, so there is an implied link from the topmost
+ * entry to the next, and so on.
+ */
 void print_memory(void);
+
+/**
+* Allocates memory for name
+*
+* @param size the size of the block
+* @param name the name to allocate memory for
+*
+* @return allocated size for the name
+*/
 void *malloc_name(size_t size, char *name);
 
 /* -- C Memory API functions -- */
+
+/**
+* Allocates memory
+*
+* @param size the size of memory to be allocated
+*
+* @return allocated size
+*/
 void *malloc(size_t size);
+
+/**
+* Frees memory
+*
+* @param ptr the pointer to the block
+*
+*/
 void free(void *ptr);
+
+/**
+* Creates memory
+*
+* @param nmemb
+* @param size the size of memory to be created
+*
+*/
 void *calloc(size_t nmemb, size_t size);
+
+/**
+* Reallocates memory
+*
+* @param ptr
+* @param size the size of memory to be reallocated
+*
+*/
 void *realloc(void *ptr, size_t size);
 
 /* -- Data Structures -- */
